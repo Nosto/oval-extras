d28e6f58cef8a2c81c387ec7e5c5058223ca2023
Vladimir Siutkin <vladimir.siutkin@nosto.com>
2025-04-08 16:36:50 +0300

    Copyright update

--- src/main/java/com/nosto/ovalextras/constraint/ValidateNestedProperty.java
+++ src/main/java/com/nosto/ovalextras/constraint/ValidateNestedProperty.java
@@ -1,13 +1,12 @@
-/*
- *  Copyright (c) 2025 Nosto Solutions Ltd All Rights Reserved.
- *
- *  This software is the confidential and proprietary information of
- *  Nosto Solutions Ltd ("Confidential Information"). You shall not
- *  disclose such Confidential Information and shall use it only in
- *  accordance with the terms of the agreement you entered into with
- *  Nosto Solutions Ltd.
- */
-
+/*******************************************************************************
+ * Copyright (c) 2025 Nosto Solutions Ltd All Rights Reserved.
+ * <p>
+ * This software is the confidential and proprietary information of
+ * Nosto Solutions Ltd ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the agreement you entered into with
+ * Nosto Solutions Ltd.
+ ******************************************************************************/
 package com.nosto.ovalextras.constraint;
 
 import net.sf.oval.configuration.annotation.Constraint;
